e9f7a357d106d0281fa9a6a6388c070c1b8332b5
Arghya <arghyajainbj@gmail.com>
2024-11-29 18:56:37 +0530

    loading

--- routes/studyPlanRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const studyPlanController = require('../controllers/studyPlanController');
-
-router.post('/generate', studyPlanController.generatePlan);
-router.get('/:planId', studyPlanController.getStudyPlan);
-router.put('/:planId', studyPlanController.updateStudyPlan);
-
-module.exports = router;
