d7a4b27c47d644c4657bd6fe3144d3fee19ce88b
Arghya <arghyajainbj@gmail.com>
2024-12-02 19:14:13 +0530

    In progress

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import authController from './controllers/authController.js';
 import chatController from './controllers/chatController.js';
-
+import quizController from './controllers/quizController.js';
 
 
 dotenv.config();
@@ -26,6 +26,7 @@ mongoose.connect(process.env.MONGODB_URI)
 // Routes
 app.use('/api/auth', authController);
 app.use('/ai',chatController)
+app.use('/study',quizController)
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
